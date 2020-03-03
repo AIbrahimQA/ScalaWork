6b8b35be3b3ac1dfc3d9cf90fcc89b44c50e320a
AIbrahimQA <ahmed.ibrahim@academytrainee.com>
2020-03-03 12:43:16 +0000

    PatternMatchingOperators

---
+++ src/main/scala/PatternMatchingOperators.scala
@@ -0,0 +1,15 @@
+object PatternMatchingOperators extends App {
+
+
+
+  val operation: Any = "Subtraction"
+
+  operation match {
+    case "Addition" => println(3 + 2)
+    case "Subtraction" => println(6-2)
+    case _ => println("some other number")
+  }
+  println(s"The Operator is $operation")
+
+
+}
