580e7351b72d508eedee9ab4b80c335c0e51e302
AIbrahimQA <ahmed.ibrahim@academytrainee.com>
2020-03-05 14:29:05 +0000

    Day4Work

--- src/main/scala/CharAt.scala
+++ src/main/scala/CharAt.scala
@@ -1,7 +1,7 @@
 object CharAt extends App {
 
 
-  val CharFind = "FindTheFifthCharacter"
+  val CharFind = "FindTheFifthCharacter "
   val charToFind = CharFind.charAt(5)
 
   println(s"The 5th character literal in $CharFind = $charToFind")
