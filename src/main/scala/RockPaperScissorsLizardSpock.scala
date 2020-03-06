ba5c79ba7b8b4939175754c05786c63b65f95541
AIbrahimQA <ahmed.ibrahim@academytrainee.com>
2020-03-06 10:20:59 +0000

    Day5Work

---
+++ src/main/scala/RockPaperScissorsLizardSpock.scala
@@ -0,0 +1,14 @@
+
+// TODO
+// Rock, Paper, Scissors, Lizard, Spock
+//Functionality: PLayer vs AI
+//Rock crushes scissors , rock crushes lizard
+//Paper covers rock, paper disproves spock
+//Scissors cuts paper, scissors decapitates lizard
+//Spock smashes scissors, spock vaporizes rock
+//Lizard eats paper , lizard poisons spock
+
+object RockPaperScissorsLizardSpock {
+
+
+}
